cc5df5e1bc9f32eca6e630b971f335511e9e25ba
Patricio Muñoz <pmherras@gmail.com>
2022-11-15 11:39:28 -0300

    agregando las sumas

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const displayValorAnterior = document.getElementById('valor-anterior');
 const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.getElementById('numero');
 const botonesOperadores = document.getElementById('operador');
+
+const Calculadora = new Calculadora();
+
+console.log(Calculadora.sumar(2,3))
+console.log(Calculadora.restar(2,3))
+console.log(Calculadora.multipiclar(2,3))
+console.log(Calculadora.dividir(2,3))
