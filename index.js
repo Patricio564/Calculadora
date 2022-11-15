878e1ac66d868bae59c3eadd44e9b92b109bd55f
Patricio Muñoz <pmherras@gmail.com>
2022-11-15 11:18:29 -0300

    estamos confirmando

---
+++ index.js
@@ -0,0 +1,4 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.getElementById('numero');
+const botonesOperadores = document.getElementById('operador');
